d65b6ddd1f10ccc814d0f8101a158a6d7f0abaec
abhinavsinha23 <abhinavsinha36@hotmail.com>
2023-07-21 17:10:46 +0100

    game + minigames

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const body = document.getElementsByTagName("body");
 const createBtn = document.getElementById("createBtn");
 const images = document.querySelectorAll("img");
 const pet = document.getElementsByName("pet");
+// const difficulty = document.getElementsByName("difficulty");
 const bottomSection = document.getElementById("bottom-section");
 const petName = document.getElementById("petName");
 const subtitle = document.getElementById("subtitle");
@@ -11,6 +13,10 @@ const funDecreaseRate = 3;
 const fitnessDecreaseRate = 2;
 const foodWaterDecreaseRate = 4;
 const energyDecreaseRate = 1;
+const music = document.getElementById("8bitMusic");
+const gameOver = document.getElementById("gameOver");
+const options = document.getElementById("options");
+const retryBtn = document.getElementById("retryBtn");
 
 let timer;
 
@@ -29,8 +35,14 @@ const foodWaterBtn = document.getElementById('foodWaterBtn');
 const energyBar = document.getElementById('energyBar');
 const energyBtn = document.getElementById('energyBtn');
 
+// decreaseTime = [300, 500, 700];
+
 selectionIndex = [];
 selectionIndex.length = pet.length;
+
+retryBtn.addEventListener("click", () => {
+    window.location.reload();
+})
 
 function checkZero(value) {
     return value == 0;
@@ -40,6 +52,7 @@ for (let i = 0; i < selectionIndex.length; i++) {
     selectionIndex[i] = 0;
 }
 createBtn.addEventListener("click", () => {
+    
     for (var i = 0; i < pet.length; i++) {
         if (pet[i].checked != true) {
             selectionIndex[i] = 0;
@@ -48,14 +61,17 @@ createBtn.addEventListener("click", () => {
             selectionIndex[i] = 1;
         }
     }
-    console.log(selectionIndex)
     if (selectionIndex.every(checkZero)) {
         alert("You haven't selected an animal!")
     }
     else if (petName.value == ""){
         alert("You haven't named your pet!")
     }
+    // else if ((difficulty[0].checked != true) && (difficulty[1].checked != true) && (difficulty[2].checked != true)){
+    //     alert("You haven't selected a difficulty level!");
+    // }
     else{
+        music.play();
         for (i = 0; i < pet.length; i++) {
             if (pet[i].checked){
                 subtitle.textContent = `${petName.value} the ${pet[i].value}`
@@ -84,14 +100,18 @@ createBtn.addEventListener("click", () => {
 
         // Function to update the stat values and bars every 10 seconds
         function decreaseStats() {
-            console.log("run")
+
             health -= healthDecreaseRate;
             if (health < 0) {
+                music.pause();
+                gameOver.play();
+                // body.style.backgroundColor = "red";
                 health = 0;
                 subtitle.textContent = `${petName.value} died`
                 for (i = 0; i < statbars.length; i++) {
                     statbars[i].remove();
                 }
+                options.style.display = "flex";
                 clearInterval(timer);
                 return;
             }
@@ -99,11 +119,15 @@ createBtn.addEventListener("click", () => {
 
             fun -= funDecreaseRate;
             if (fun < 0) {
+                music.pause();
+                gameOver.play();
+                // body.style.backgroundColor = "red";
                 fun = 0;
                 subtitle.textContent = `${petName.value} is bored`
                 for (i = 0; i < statbars.length; i++) {
                     statbars[i].remove();
                 }
+                options.style.display = "flex";
                 clearInterval(timer);
                 return;
             }
@@ -111,11 +135,15 @@ createBtn.addEventListener("click", () => {
 
             fitness -= fitnessDecreaseRate;
             if (fitness < 0) {
+                music.pause();
+                gameOver.play();
+                // body.style.backgroundColor = "red";
                 fitness = 0;
                 subtitle.textContent = `${petName.value} is unfit`
                 for (i = 0; i < statbars.length; i++) {
                     statbars[i].remove();
                 }
+                options.style.display = "flex";
                 clearInterval(timer);
                 return;
             }
@@ -123,11 +151,15 @@ createBtn.addEventListener("click", () => {
 
             foodWater -= foodWaterDecreaseRate;
             if (foodWater < 0) {
+                music.pause();
+                gameOver.play();
+                // body.style.backgroundColor = "red";
                 foodWater = 0;
                 subtitle.textContent = `${petName.value} died from starvation/dehydration`
                 for (i = 0; i < statbars.length; i++) {
                     statbars[i].remove();
                 }
+                options.style.display = "flex";
                 clearInterval(timer);
                 return;
             }
@@ -135,11 +167,15 @@ createBtn.addEventListener("click", () => {
 
             energy -= energyDecreaseRate;
             if (energy < 0) {
+                music.pause();
+                gameOver.play();
+                // body.style.backgroundColor = "red";
                 energy = 0;
                 subtitle.textContent = `${petName.value} has no energy`
                 for (i = 0; i < statbars.length; i++) {
                     statbars[i].remove();
                 }
+                options.style.display = "flex";
                 clearInterval(timer);
                 return;
             }
@@ -185,9 +221,14 @@ createBtn.addEventListener("click", () => {
 
         // Run the function to update the decrease rates on page load
         updateDecreaseRates();
-        
+    
         // Start decreasing stats after 10 seconds
-        timer = setInterval(decreaseStats, 400);
+
+
+        timer = setInterval(decreaseStats, 350);
+
+    
+            
 
             }
         })
